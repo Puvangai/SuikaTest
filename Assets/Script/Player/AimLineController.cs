a0cbb3ff112a377b04516e1327cab416d6d22c10
Burak Efe Özçiftci <ozciftciburakefe@gmail.com>
2026-08-04 22:44:37 +0300

    Controller Script Update
    Line scritpi yazıldı ama player controllerda bir sıkıntı var play atınca girdi alınan ınputlar çalışmıyor bugün çözmeye çalışırım olmazsa yarın burdan başlıyacağım.

--- Assets/Script/Player/AimLineController.cs
+++ Assets/Script/Player/AimLineController.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class AimLineController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private Transform _fruitThrowTransform; // Fruit'in pozisyonunu temsil eden Transform referansı
+    [SerializeField] private Transform _bottomTransform; // Çizginin alt kısmını temsil eden Transform referansı
+
+    //Line Renderer componentini tutacak değişken
+    private LineRenderer _lineRenderer;
+
+
+    private float _topPos; //Line Renderer'ın üst pozisyonu
+    private float _bottomPos; //Line Renderer'ın alt pozisyonu
+    private float _x; //Line Renderer'ın bulunduğu x pozisyonu
+
+
+
+    private void Awake()
     {
-        
+        _lineRenderer = GetComponent<LineRenderer>(); // Line Renderer componentini al atama yapıyoruz
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _x = _fruitThrowTransform.position.x; // Fruit'in x pozisyonunu alıyoruz
+        _topPos = _fruitThrowTransform.position.y; // Fruit'in üst pozisyonunu alıyoruz
+        _bottomPos = _bottomTransform.position.y; //Fruit'in alt pozisyonunu alıyoruz
+
+
+        _lineRenderer.SetPosition(0, new Vector3(_x, _topPos)); //Line Renderer'ın üst pozisyonunu ayarlıyoruz
+        _lineRenderer.SetPosition(1, new Vector3(_x, _bottomPos)); //Line Renderer'ın alt pozisyonunu ayarlıyoruz
+    }
+
+
+    private void OnValidate() //Bu metod, Unity editöründe değişiklik yapıldığında çağrılır ve Line Renderer'ın pozisyonlarını günceller + olarak bu metod oyun çalışmazjen bile çalışır.
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _x = _fruitThrowTransform.position.x;
+        _topPos = _fruitThrowTransform.position.y;
+        _bottomPos = _bottomTransform.position.y;
+
+
+        _lineRenderer.SetPosition(0, new Vector3(_x, _topPos));
+        _lineRenderer.SetPosition(1, new Vector3(_x, _bottomPos));
     }
 }
