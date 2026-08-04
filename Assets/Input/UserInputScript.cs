8c144e6591abc56ec01a19006ad2eedb92be362f
Burak Efe Özçiftci <ozciftciburakefe@gmail.com>
2026-08-04 12:25:14 +0300

    InputScript ve PlayerControllerScript
    2 script ekledim ufak tefek gereksinimleride ekledim şimdi scriptlere devam etmeden önce sprite görüntü düzeltme işine bir bakıcam

--- Assets/Input/UserInputScript.cs
+++ Assets/Input/UserInputScript.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class UserInputScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public static PlayerInput playerInput;
+
+    public static Vector2 MoveInput { get; set; }
+
+    public static bool IsThrowPressed { get; set; }
+
+    private InputAction _moveAction;
+    private InputAction _throwAction;
+
+    void Awake()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        _moveAction = playerInput.actions["Move"];
+        _throwAction = playerInput.actions["Throw"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MoveInput = _moveAction.ReadValue<Vector2>();
+        IsThrowPressed = _throwAction.WasPressedThisFrame();
     }
+
 }
---
+++ Assets/Script/Player/PlayerControllerScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+
+    [SerializeField] private float _movespeed = 5f;
+    [SerializeField] private BoxCollider2D _boundaries;
+    [SerializeField] private Transform _fruitThrowTransform;
+
+    private Bounds _bounds;
+
+    private float _leftBound;
+    private float _rightBound;
+
+    private float _startingLeftBound;
+    private float _startingRightBound;
+
+    private float _offset;
+
+
+    private void Awake()
+    {
+        _bounds = _boundaries.bounds;
+
+        _offset = transform.position.x - _fruitThrowTransform.position.x;
+
+
+        _leftBound = _bounds.min.x + _offset;
+        _rightBound = _bounds.max.x + _offset;
+
+
+        _startingLeftBound = _leftBound;
+        _startingRightBound = _rightBound;
+    }
+
+    private void Update()
+    {           
+        Vector3 newPosition = transform.position + new Vector3(UserInputScript.MoveInput.x * _movespeed * Time.deltaTime, 0f, 0f);
+        newPosition.x = Mathf.Clamp(newPosition.x, _leftBound, _rightBound);
+
+       transform.position = newPosition;
+    }
+
+}
