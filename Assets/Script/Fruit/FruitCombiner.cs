4bf41d34db260a14dab8e329aeb2d8700a942f4a
Burak Efe Özçiftci <ozciftciburakefe@gmail.com>
2026-08-07 10:25:08 +0300

    Completed Version

--- Assets/Script/Fruit/FruitCombiner.cs
+++ Assets/Script/Fruit/FruitCombiner.cs
@@ -1,6 +1,65 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
 
 public class FruitCombiner : MonoBehaviour
 {
 
+    private int _layerIndex;
+
+    private FruitInfo _info;
+
+    private void Awake()
+    {
+        _layerIndex = gameObject.layer;
+        _info = GetComponent<FruitInfo>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == _layerIndex)
+        {
+            FruitInfo info = collision.gameObject.GetComponent<FruitInfo>();
+            if (info != null)
+            {
+                if (info.FruitIndex == _info.FruitIndex)
+                {
+                    int thisID = gameObject.GetInstanceID();
+                    int otherID = collision.gameObject.GetInstanceID();
+
+                    if (thisID > otherID)
+                    {
+                        if (_info.FruitIndex == FruitSelector.Instance.Fruits.Length - 1)
+                        {
+                            Destroy(collision.gameObject);
+                            Destroy(gameObject);
+                        }
+
+                        else
+                        {
+                            Vector3 middlePosition = (transform.position + collision.transform.position) / 2f;
+                            GameObject go =  Instantiate(SpawnCombinedFruit(_info.FruitIndex), GameManager.Instance.transform);
+                            go.transform.position = middlePosition;
+
+                            ColliderInformer informer = go.GetComponent<ColliderInformer>();
+                            if (informer != null)
+                            {
+                                informer.WasCombineIn = true;
+                            }
+
+                            Destroy(collision.gameObject);
+                            Destroy(gameObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private GameObject SpawnCombinedFruit(int index) 
+    {
+        GameObject go = FruitSelector.Instance.Fruits[index + 1];
+        return go;
+    }
 }
--- Assets/Script/Fruit/FruitInfo.cs
+++ Assets/Script/Fruit/FruitInfo.cs
@@ -6,16 +6,19 @@ public class FruitInfo : MonoBehaviour
     public int PointWhenAnnihilated;
     public float FruitMass = 1f;
 
-    private Rigidbody _rb;
+    // 2D oyunlar için Rigidbody2D kullanıyoruz
+    private Rigidbody2D _rb;
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
-        _rb.mass = FruitMass;    
+        // 1. 2D Fizik bileşenini alıyoruz
+        _rb = GetComponent<Rigidbody2D>();
+
+        // 2. Güvenlik Kontrolü: Eğer obje üzerinde Rigidbody2D varsa kütleyi ayarla.
+        // Bu sayede fiziksiz bir meyveye (NoPyhsicFruit) eklense bile oyun patlamaz.
+        if (_rb != null)
+        {
+            _rb.mass = FruitMass;
+        }
     }
-
-
-
-
-
 }
