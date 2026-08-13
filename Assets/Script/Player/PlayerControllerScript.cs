a7f57b4958c06f7c3ac0f6c7940700dfff729385
Burak Efe Özçiftci <ozciftciburakefe@gmail.com>
2026-08-13 15:38:21 +0300

    Completed Controller Fix

--- Assets/Script/Player/PlayerControllerScript.cs
+++ Assets/Script/Player/PlayerControllerScript.cs
@@ -39,80 +39,21 @@ public class PlayerController : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = _mainCamera.ScreenToWorldPoint(touch.position);
-            Vector3 newPosition = new Vector3(touchPosition.x + _offset, transform.position.y, transform.position.z);
-            newPosition.x = Mathf.Clamp(newPosition.x, _leftBound, _rightBound);
-            transform.position = newPosition;
+            NewMethod(touch.position);
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            print("Mouse'a basıldı!");
-            Mouse mouse = Mouse.current;
-            Vector2 screenPos = mouse.position.ReadValue();
-            Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, _mainCamera.nearClipPlane));
-            transform.position = worldPosition;
+            NewMethod(Input.mousePosition);
         }
-        else
-        {
-            print("Mouse'a basılma algılanmadı.");
-            return;
-        }
+    }
 
-        //else
-        //{
-        //    Vector2 input = UserInput.MoveInput;
-        //}
-
-
-
-        //Vector2 input = UserInput.MoveInput;
-
-        //// =======================================================
-        //// ANDROID / TOUCH
-        //// Parmağın ekran üzerindeki X pozisyonunu direkt takip eder.
-        //// =======================================================
-        //if (Mathf.Abs(input.x) > 1f || Mathf.Abs(input.y) > 1f)
-        //{
-        //    Vector3 worldPos = _mainCamera.ScreenToWorldPoint(
-        //        new Vector3(input.x, input.y, _mainCamera.nearClipPlane)
-        //    );
-
-        //    float targetX = Mathf.Clamp(
-        //        worldPos.x,
-        //        _leftBound,
-        //        _rightBound
-        //    );
-
-        //    transform.position = new Vector3(
-        //        targetX,
-        //        transform.position.y,
-        //        transform.position.z
-        //    );
-        //}
-
-        //// =======================================================
-        //// PC / GAMEPAD
-        //// Input değerine göre hareket eder.
-        //// =======================================================
-        //else if (Mathf.Abs(input.x) > 0.01f)
-        //{
-        //    float movement = input.x * _movespeed * Time.deltaTime;
-
-        //    float targetX = transform.position.x + movement;
-
-        //    targetX = Mathf.Clamp(
-        //        targetX,
-        //        _leftBound,
-        //        _rightBound
-        //    );
-
-        //    transform.position = new Vector3(
-        //        targetX,
-        //        transform.position.y,
-        //        transform.position.z
-        //    );
-        //}
+    private void NewMethod(Vector3 pos)
+    {
+        Vector3 mouseWorldPosition = _mainCamera.ScreenToWorldPoint(pos);
+        Vector3 newPosition = new Vector3(mouseWorldPosition.x + _offset, transform.position.y, transform.position.z);
+        newPosition.x = Mathf.Clamp(newPosition.x, _leftBound, _rightBound);
+        transform.position = newPosition;
     }
 
     public void ChangeBoundary(float extraWidth)
